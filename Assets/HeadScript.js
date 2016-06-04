5fd02aa7f81527f714425c99e142fc94d6476b21
LithiumCore <qwerty526526@gmail.com>
2016-06-04 11:57:18 -0400

    assets commited

---
+++ Assets/HeadScript.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+var body : GameObject;
+var fixLocation : Vector3;
+
+function Start () {
+
+}
+
+function Update () {
+    transform.position = Vector3(body.transform.position.x, body.transform.position.y + .7901, body.transform.position.z);       
+}
